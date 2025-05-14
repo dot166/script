43ef72e46d27f1c524be2be2f5c73b52b7dea840
._______166 <retromac9x2@gmail.com>
2025-05-14 14:56:44 +0100

    use config file again

--- jOS/source/ci-tool/src/main.rs
+++ jOS/source/ci-tool/src/main.rs
@@ -1,5 +1,6 @@
+use lib_aosp::scripts;
 use std::path::{Path, PathBuf};
-use std::process::{exit, Command};
+use std::process::Command;
 use std::{env, fs, thread, time};
 
 pub fn get_script_dir() -> Option<PathBuf> {
@@ -57,20 +58,46 @@ fn main() {
     if !status.success() {
         panic!("Failed to auth with token on CI");
     }
+    let (graphene_tag, graphene_tag_old, _) = scripts::read_config_file();
     fs::create_dir("tmp").unwrap();
     env::set_current_dir("tmp").unwrap();
     let status = Command::new("../../manage").arg("init").status().unwrap();
     if !status.success() {
         panic!("Failed to run init script");
     }
-    let status = Command::new("../../manage").arg("update").status().unwrap();
-    if !status.success() {
-        panic!("Failed to rebase!!! source tree may be corrupted!!");
+    if (graphene_tag.parse::<i32>().unwrap() > graphene_tag_old.parse::<i32>().unwrap()) {
+        let status = Command::new("../../manage").arg("update").status().unwrap();
+        if !status.success() {
+            panic!("Failed to rebase!!! source tree may be corrupted!!");
+        }
+        let config_path: &str;
+        config_path = "../../../../common";
+        let path = env::current_exe().unwrap().parent().unwrap().join(config_path);
+        let contents = fs::read_to_string(&path).expect("Failed to read common.sh");
+        fs::write(path, contents.replace(&graphene_tag_old, &*graphene_tag)).expect("Failed to update config file");
     }
     env::set_current_dir(&Path::new("..")).unwrap();
     fs::remove_dir_all("tmp").unwrap();
     let status = Command::new("bash").arg("-c").arg("gh auth logout").status().unwrap();
     if !status.success() {
         panic!("Failed to deauth on CI");
+    }
+    let status = Command::new("git")
+        .arg("commit")
+        .arg("../../.")
+        .arg("-m")
+        .arg(format!("GrapheneOS {} rebase complete", graphene_tag))
+        .status();
+
+    if let Err(e) = status {
+        panic!("Error committing changes: {}", e);
+    }
+
+    let status = Command::new("git")
+        .arg("push")
+        .status();
+
+    if let Err(e) = status {
+        panic!("Error pushing changes: {}", e);
     }
 }
