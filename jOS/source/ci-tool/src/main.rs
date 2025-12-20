ed72bfd906f68cf802c9b61f96413315231bfb4d
._______166 <retromac9x2@gmail.com>
2025-12-21 07:23:18 +0900

    remove scripts for forking AOSP repos
    all the things i need to fork are in graphene anyway

--- jOS/source/ci-tool/src/main.rs
+++ jOS/source/ci-tool/src/main.rs
@@ -23,7 +23,7 @@ pub fn get_script_dir() -> Option<PathBuf> {
 fn main() {
     unsafe { env::set_var("IS_CI", "true"); }
     env::set_current_dir(&get_script_dir().unwrap()).expect("Failed to change directory");
-    let scripts = ["build-android", "emoji", "fork-aosp", "manage", "update-checkout"];
+    let scripts = ["build-android", "emoji", "manage", "update-checkout"];
     for script in scripts.iter() {
         println!("Building {}", script);
         env::set_current_dir(&Path::new(script)).unwrap();
